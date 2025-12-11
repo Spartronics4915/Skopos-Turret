6c91c3b361296597936da3818809f9133f4c4b03
Goblin <elliotbrower@gmail.com>
2025-12-10 19:28:57 -0800

    Required Odometry related imports for swerve

--- src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
@@ -2,6 +2,10 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
