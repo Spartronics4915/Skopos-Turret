a60e51963f0350e3ad33b71126bc1452894c571f
Ehren von Ibsch <ehrenwolf42@icloud.com>
2025-12-31 12:30:58 -0800

    Create maybe working basic vision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,7 +14,14 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.AngularVelocity;
@@ -188,5 +195,14 @@ public final class Constants {
         public static final Distance INTAKE_LC_TRIGGER_DISTANCE = Meters.of(0.109);
         public static final double INTAKE_LC_DEBOUNCE = 0.03;
         public static final double INTAKE_TIMEOUT = 3;
+    }
+
+    public static final class VisionConstants {
+        public static final Transform3d ROBOT_TO_CAMERA = new Transform3d(
+            new Translation3d(0, 0, 0),
+            new Rotation3d(0, 0, 0)
+        );
+        public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(0, 0, 0);
+        public static final Matrix<N3, N1> MULTI_TAG_STD_DEVS = VecBuilder.fill(0, 0, 0);
     }
 }   
---
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -0,0 +1,98 @@
+package frc.robot.subsystems.vision;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import static org.photonvision.PhotonPoseEstimator.PoseStrategy.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.VisionConstants.*;
+
+public class VisionSubsystem extends SubsystemBase {
+
+    private PhotonCamera camera;
+    private PhotonPoseEstimator estimator;
+    private List<PhotonPipelineResult> results;
+    private Optional<EstimatedRobotPose> currentResultPose;
+    private Matrix<N3, N1> currentStdDevs;
+    private final EstimateConsumer estimateConsumer;
+
+    public VisionSubsystem(EstimateConsumer estimateConsumer) {
+        this.estimateConsumer = estimateConsumer;
+        camera = new PhotonCamera("daniil");
+        estimator = new PhotonPoseEstimator(
+            AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo), 
+            PNP_DISTANCE_TRIG_SOLVE, 
+            ROBOT_TO_CAMERA
+        );
+    }
+
+    @Override
+    public void periodic() {
+        results = camera.getAllUnreadResults();
+        for (PhotonPipelineResult currentResult : results) {
+            currentResultPose = estimator.update(currentResult);
+            updateEstimationStdDevs(currentResultPose, currentResult.getTargets());
+            currentResultPose.ifPresent(
+                est -> {
+                    var estStdDevs = getEstimationStdDevs();
+                    estimateConsumer.accept(est.estimatedPose.toPose2d(), est.timestampSeconds, estStdDevs);
+                }
+            );
+        }
+    }
+
+    private void updateEstimationStdDevs(Optional<EstimatedRobotPose> estimatedPose, List<PhotonTrackedTarget> targets) {
+        if (estimatedPose.isEmpty()) currentStdDevs = SINGLE_TAG_STD_DEVS;
+            else {
+                Matrix<N3, N1> estimateStdDevs = SINGLE_TAG_STD_DEVS;
+                int numTags = 0;
+                double avgDist = 0;
+                for (PhotonTrackedTarget trackedTarget : targets) {
+                    Optional<Pose3d> tagPose = estimator.getFieldTags().getTagPose(trackedTarget.getFiducialId());
+                    if (tagPose.isEmpty()) continue;
+                    numTags++;
+                    avgDist += tagPose
+                        .get()
+                        .toPose2d()
+                        .getTranslation()
+                        .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
+                }
+                if (numTags == 0) currentStdDevs = SINGLE_TAG_STD_DEVS;
+                    else {
+                        avgDist /= numTags;
+                        if (numTags > 1) estimateStdDevs = MULTI_TAG_STD_DEVS;
+                        if (numTags == 1 && avgDist > 4) estimateStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+                            else estimateStdDevs = estimateStdDevs.times(1 + (avgDist * avgDist / 30));
+                        currentStdDevs = estimateStdDevs;
+                    }
+            }
+    }
+
+    public Matrix<N3, N1> getEstimationStdDevs() {
+        return currentStdDevs;
+    }
+
+    public Pose3d getPose3d(EstimatedRobotPose estimatedRobotPose) {
+        return estimatedRobotPose.estimatedPose;
+    }
+
+    @FunctionalInterface
+    public static interface EstimateConsumer {
+        public void accept(Pose2d pose, double timestamp, Matrix<N3, N1> estimationStdDevs);
+    }
+}
